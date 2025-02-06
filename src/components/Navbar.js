d2e81a2173199ccc207aa5bcfd311c6d31bb75d9
papahmwe <doublehmwenetmsi@gmail.com>
2025-02-06 15:58:18 +0630

    feat: added_latest

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,50 +2,41 @@ import Image from "next/image";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center py-4 px-8 shadow-md bg-white font-jost relative">
-
-      <Link href="/">
-        <Image src="/images/logo.svg" alt="Daily Bloom Logo" width={100} height={100 / 3} />
-      </Link>
-
-      <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? <X size={28} /> : <Menu size={28} />}
-      </button>
-
-      <div
-        className={`absolute top-16 right-4 w-60 bg-white shadow-lg rounded-lg flex flex-col items-start p-4 space-y-4 transition-transform ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        } md:hidden`}
-      >
-        <NavLink href="/" pathname={pathname} label="Home" onClick={() => setIsOpen(false)} />
-        <NavLink href="/about" pathname={pathname} label="About" onClick={() => setIsOpen(false)} />
-        <NavLink href="/contact" pathname={pathname} label="Contact" onClick={() => setIsOpen(false)} />
-
-    
-        <NavLink
-          href="/sign-up"
-          pathname={pathname}
-          label="Sign Up"
-          className="border border-theme-dark text-theme-dark uppercase px-4 py-2 rounded-lg w-full text-center"
-          onClick={() => setIsOpen(false)}
-        />
-        <NavLink
-          href="/login"
-          pathname={pathname}
-          label="Login"
-          className="bg-theme-dark text-white uppercase px-4 py-2 rounded-lg w-full text-center"
-          onClick={() => setIsOpen(false)}
+    <div className="w-[1440px] h-[165px] mx-auto flex justify-center items-center gap-[300px]">
+      {/* Logo */}
+      <div>
+        <Image
+          src="/assets/home/logo.png"
+          alt="Nav_Logo"
+          width={156}
+          height={105.49}
+          className="object-cover"
         />
       </div>
 
-      <div className="hidden md:flex space-x-6 items-center">
-        <NavLink href="/" pathname={pathname} label="Home" />
-        <NavLink href="/about" pathname={pathname} label="About Us" />
-        <NavLink href="/contact" pathname={pathname} label="Contact Us" />
+      {/* Navbar */}
+      <div className="w-[855px] h-[76px] flex justify-between items-center gap-[40px]">
+        <ul className="flex justify-center items-center gap-[36px] ">
+          <li className="w-[84px] h-[36px] font-jost font-[700] text-[24px] text-[#8174FF] uppercase leading-[34.68px]">
+            Home
+          </li>
+          <li className="w-[84px] h-[36px] font-jost font-[700] text-[24px] text-[#000000] uppercase leading-[34.68px]">
+            About
+          </li>
+          <li className="w-[84px] h-[36px] font-jost font-[700] text-[24px] text-[#000000] uppercase leading-[34.68px]">
+            Contact
+          </li>
+        </ul>
 
-
-        <NavLink href="/sign-up" pathname={pathname} label="Sign Up" className="border border-theme-dark text-theme-dark uppercase px-4 py-2 rounded-lg" />
-        <NavLink href="/login" pathname={pathname} label="Login" className="bg-theme-dark text-white uppercase px-4 py-2 rounded-lg" />
+        {/* Buttons */}
+        <div className="flex justify-center items-center gap-[36px]">
+          <button className="w-[168px] h-[52px] font-montserrat font-[600] text-[23px] text-mainPrimary uppercase leading-[28.04px] border outline-none border-mainPrimary rounded-[10px]">
+            Sign Up
+          </button>
+          <button className="w-[168px] h-[52px] font-montserrat font-[600] text-[23px] text-backgroundPrimary uppercase leading-[28.04px] border outline-none bg-mainPrimary rounded-[10px]">
+            login
+          </button>
+        </div>
       </div>
     </div>
   );

790290f7968856d99f34222fb00bc5c63f7bf26d
papahmwe <doublehmwenetmsi@gmail.com>
2025-02-10 13:15:42 +0630

    Revert "feat: added_home&modified-about"
    This reverts commit ca7e9f5eba7150911eadca265cb4b844eab57398.

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,13 +1,5 @@
 // Landing Page ("/")
 
-import Hero from "@/components/Home/Hero";
-import HeroFooter from "@/components/Home/Hero_footer";
-
 export default function Home() {
-  return (
-    <div>
-      <Hero />
-      <HeroFooter />
-    </div>
-  );
+  return <div></div>;
 }

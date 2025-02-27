45acb0dc4d633b8747a79891a0bafd2d55426788
Noel Paing Oak Soe <u6530183@au.edu>
2025-02-27 15:02:20 +0700

    fix: challenge model

--- src/app/api/challenges/create/route.js
+++ src/app/api/challenges/create/route.js
@@ -2,24 +2,35 @@ import { NextResponse } from 'next/server'
 import Challenge from '../../../../../models/Challenge'
 import connectDB from '../../../../lib/db'
 
-
 export async function POST(request) {
-    try {
-        await connectDB()
-        const body = await request.json()
-        const { name, duration, time_of_day, date_to_do, user, challenge_img } = body
-        const challenge = new Challenge({ name, duration, time_of_day, date_to_do, user, challenge_img })
-        await challenge.save()
-        return NextResponse.json(challenge, { status: 201 })
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
-    }
+  try {
+    await connectDB()
+    const body = await request.json()
+    const {
+      name,
+      duration,
+      time,
+      time_period,
+      notification,
+      date_to_do,
+      user,
+      challenge_img,
+    } = body
+    const challenge = new Challenge({
+      name,
+      duration,
+      time,
+      time_period,
+      notification,
+      date_to_do,
+      user,
+      challenge_img,
+    })
+    await challenge.save()
+    return NextResponse.json(challenge, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
 }
 
-
 // postman request
-
-
-
-
-
